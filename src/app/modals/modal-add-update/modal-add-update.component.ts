57c4c0af50377c99058e8162a08b62d26113d42e
Pedro de León <38962557+Turtugilla@users.noreply.github.com>
2020-04-13 15:38:19 -0500

    updated

--- src/app/modals/modal-add-update/modal-add-update.component.ts
+++ src/app/modals/modal-add-update/modal-add-update.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Automovil } from 'src/app/models';
+import { AutosService } from 'src/app/autos.service';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-modal-add-update',
@@ -13,12 +15,40 @@ export class ModalAddUpdateComponent implements OnInit {
   accion: string;
 
   auto: Automovil = {} as Automovil;
+  desde: number;
+  hasta: number;
 
   constructor(public activeModal: NgbActiveModal) {
   }
 
   ngOnInit() {
-
+    if (this.accion === 'Editar') {
+      let lengthModelos: number = this.auto.modelos.length;
+      this.desde = this.auto.modelos[0];
+      this.hasta = this.auto.modelos[lengthModelos - 1];
+    }
   }
+
+
+  onSubmit(formAutos) {
+    let modelos: number[] = this.ensamblajeModelos();
+    this.auto.modelos = modelos;
+    this.activeModal.close(this.auto);
+  }
+
+
+  private ensamblajeModelos(): number[] {
+    let modelos: number[];
+    modelos = [];
+
+
+
+    for (let cont = this.desde; cont <= this.hasta; cont++) {
+      modelos.push(cont);
+    }
+    return modelos;
+  }
+
+
 
 }
